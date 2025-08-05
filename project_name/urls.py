b2e0b56faa9ad3ed64b7565cb8adcc61fa133184
jaca <vitouejj@gmail.com>
2025-08-05 19:00:05 +0200

    add custom error pages for http 401, 403, 404 and 500 errors

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from {{ project_name }}.views import home
+from {{ project_name }}.views import (
+    home,
+    handle_error_401,
+    handle_error_403,
+    handle_error_404,
+    handle_error_500,
+)
 
 urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
+
+handler401 = handle_error_401
+handler403 = handle_error_403
+handler404 = handle_error_404
+handler500 = handle_error_500
+
--- project_name/views.py
+++ project_name/views.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.http import require_GET
@@ -11,3 +13,44 @@ def home(request: HttpRequest) -> HttpResponse:
         request,
         template_name='home.html',
     )
+
+
+def handle_error_401(
+    request: HttpRequest,
+    exception: BaseException,
+) -> HttpResponse:
+    return render(
+        request,
+        template_name='401.html',
+    )
+
+
+def handle_error_403(
+    request: HttpRequest,
+    exception: BaseException,
+) -> HttpResponse:
+    return render(
+        request,
+        template_name='403.html',
+        status=HTTPStatus.FORBIDDEN,
+    )
+
+
+def handle_error_404(
+    request: HttpRequest,
+    exception: BaseException,
+) -> HttpResponse:
+    return render(
+        request,
+        template_name='404.html',
+        status=HTTPStatus.NOT_FOUND,
+    )
+
+
+def handle_error_500(request: HttpRequest) -> HttpResponse:
+    return render(
+        request,
+        template_name='500.html',
+        status=HTTPStatus.INTERNAL_SERVER_ERROR,
+    )
+
